58eda1a9e04d5b041ad0b73b527187aff4537832
ThatCasual <that.casual@outlook.com>
2022-12-01 11:31:47 -0800

    2nd try.

---
+++ src/main/java/net/thatcasual/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.thatcasual.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
